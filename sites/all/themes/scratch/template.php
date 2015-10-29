30d77b20cc149439b9f0f1bae0e49c79420dcd6e
Ashlin <ashlinaronin@gmail.com>
2015-10-29 14:42:29 -0700

    Add scratch theme

---
+++ sites/all/themes/scratch/template.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+** Implements hook_links__system_main_menu().
+** Try changing main menu to an ordered list -- works.
+*/
+// function scratch_links__system_main_menu($variables) {
+//   $html = "<div>\n";
+//   $html .= " <ol>\n";
+//
+//   foreach ($variables['links'] as $link) {
+//     $html .= "<li class='ordered-li'>" . l($link['title'], $link['href'], $link) . "</li>";
+//   }
+//   $html .= " </ol>\n";
+//   $html .= "</div>\n";
+//
+//   return $html;
+// }
